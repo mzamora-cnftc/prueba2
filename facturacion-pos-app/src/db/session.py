6b081e74ec97cb683cdcd3f8c6f82d9d4f845746
mauricio-zamora <141055068+mzamora-cnftc@users.noreply.github.com>
2025-07-23 16:24:52 +0000

    init 2

--- facturacion-pos-app/src/db/session.py
+++ facturacion-pos-app/src/db/session.py
@@ -5,11 +5,11 @@ from sqlalchemy.ext.declarative import declarative_base
 DATABASE_URL = "sqlite:///facturacion.db"  # Cambiar a la URL de MariaDB si es necesario
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
-SessionLocal = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
+Session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))  # <-- Cambiado a 'Session'
 Base = declarative_base()
 
 def get_db():
-    db = SessionLocal()
+    db = Session()
     try:
         yield db
     finally:
